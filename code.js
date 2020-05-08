5069652935ffeb3d8303f7bc8c7966e8aa566323
Danoneko <theicewone@mail.ru>
2020-05-08 16:48:10 +0300

    7

--- code.js
+++ code.js
@@ -64,8 +64,8 @@ var arrayEdit = [];
 					else{
 						
 						if((columns == 10 || columns == 9) && (screen.width <= 1000)){
-							$('table td').css('width','70px');
-							$('table td').css('height','70px');
+							$('table td').css('width','50px');
+							$('table td').css('height','50px');
 						}
 						
 						arrayEdit = [];
